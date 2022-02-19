bc7c83f887b31fe09e8da28f76e29d3db21081d1
Harshit Khanna <harshitkhanna3254@gmail.com>
2022-02-19 17:54:18 +0530

    lps

--- strings/pattern/KMP/LPSArray.java
+++ strings/pattern/KMP/LPSArray.java
@@ -1,5 +1,6 @@
 package strings.pattern.KMP;
 
+import java.util.Arrays;
 
 // string = "abacabad"
 // lpsArr = {0, 0, 1, 0, 1, 2, 3, 0 }
@@ -10,14 +11,35 @@ package strings.pattern.KMP;
 public class LPSArray {
     public static void main(String[] args) {
 
-        String str = "abacabad";
-        lpsArray(str);
+        String str = "aaabaaaac";
+        int[] lpsArr = new int[str.length()];
+
+        System.out.println(Arrays.toString(lpsArray(str, lpsArr)));
     }
 
-    public static int[] lpsArray(String str) {
+    public static int[] lpsArray(String str, int[] lpsArr) {
 
-        int[] arr = { 0, 1, 4 };
-        return arr;
+        int n = str.length();
+        lpsArr[0] = 0;
 
+        int len = 0;
+        int i = 1;
+
+        while (i < n) {
+            if (str.charAt(i) == str.charAt(len)) {
+                len++;
+                lpsArr[i] = len;
+                i++;
+            } else {
+                if (len == 0) {
+                    lpsArr[i] = 0;
+                    i++;
+                } else {
+                    len = lpsArr[len - 1];
+                }
+            }
+        }
+
+        return lpsArr;
     }
 }
