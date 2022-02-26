1cdbed975c3df7c63110acdeac1adfab4b264697
Harshit Khanna <harshitkhanna3254@gmail.com>
2022-02-26 17:35:08 +0530

    Update SimpleLinkedList.java

--- linkedlist/SimpleLinkedList.java
+++ linkedlist/SimpleLinkedList.java
@@ -14,18 +14,95 @@ public class SimpleLinkedList {
 
     public static void main(String[] args) {
 
-        Node head = new Node(10);
-        Node node2 = new Node(20);
+        Node head = new Node(20);
+        Node node2 = new Node(25);
         Node node3 = new Node(30);
 
         head.next = node2;
         node2.next = node3;
 
         print(head);
+        head = insertBegin(head, 15);
+        head = insertBegin(head, 10);
         head = insertBegin(head, 5);
         print(head);
+        head = insertEnd(head, 35);
         head = insertEnd(head, 40);
+        head = insertEnd(head, 45);
         print(head);
+        head = deleteFirst(head);
+        print(head);
+        head = deleteLast(head);
+        print(head);
+        head = insertAtPosition(head, 5, 1);
+        print(head);
+        head = insertAtPosition(head, 5, 14);
+        print(head);
+        System.out.println("Value 40 found at position: " + searchRecursive(head, 40, 1));
+        System.out.println("Value 10 found at position: " + searchRecursive(head, 10, 1));
+        System.out.println("Value 70 not found " + searchRecursive(head, 700, 1));
+
+    }
+
+    public static int searchRecursive(Node head, int data, int position) {
+
+        if (head == null) {
+            return -1;
+        }
+
+        if (head.data == data) {
+            return position;
+        }
+
+        return searchRecursive(head.next, data, position + 1);
+    }
+
+    // position 2 = index 1
+    public static Node insertAtPosition(Node head, int data, int position) {
+        Node newNode = new Node(data);
+
+        if (position == 1) {
+            newNode.next = head;
+            return newNode;
+        }
+
+        Node curr = head;
+
+        int i = 1;
+        while (i != position - 1 && curr != null) {
+            curr = curr.next;
+            i++;
+        }
+
+        if (curr == null) {
+            System.out.println("bad position");
+            return head;
+        }
+
+        newNode.next = curr.next;
+        curr.next = newNode;
+
+        return head;
+    }
+
+    public static Node deleteLast(Node head) {
+        Node temp = head;
+
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+
+        return head;
+    }
+
+    public static Node deleteFirst(Node head) {
+        Node temp = head;
+        head = head.next;
+        temp.next = null;
+
+        return head;
     }
 
     public static Node insertEnd(Node head, int data) {
@@ -33,16 +110,15 @@ public class SimpleLinkedList {
         Node newNode = new Node(data);
 
         if (head == null) {
-            head = newNode;
-        } else {
-            Node temp = head;
-
-            while (temp.next != null) {
-                temp = temp.next;
-            }
-
-            temp.next = newNode;
+            return newNode;
         }
+        Node temp = head;
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
 
         return head;
     }
